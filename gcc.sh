1cf06ac968cea3bbdab854b1a20147efd1ef2ac6
wdonadelli <wdonadelli@gmail.com>
2020-02-22 01:12:53 -0300

    Em processo de criação...

--- gcc.sh
+++
@@ -1,7 +0,0 @@
-clear;
-
-gcc `pkg-config --cflags gtk+-3.0` -c libuigtk.c `pkg-config --libs gtk+-3.0`;
-gcc `pkg-config --cflags gtk+-3.0` -c example.c  `pkg-config --libs gtk+-3.0`;
-gcc `pkg-config --cflags gtk+-3.0` -o example example.o libuigtk.o `pkg-config --libs gtk+-3.0`;
-
-./example;
